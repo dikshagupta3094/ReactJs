aa5c27dc22b6b6e2b04e828e31cfd29ddb2573b8
dikshagupta3094 <dikshagpragya@gmail.com>
2023-10-17 06:52:43 +0530

    create password Generator

--- 06_passwordGenerator/src/app.jsx
+++ 06_passwordGenerator/src/app.jsx
@@ -1,37 +1,75 @@
-import { useState , useCallback } from 'preact/hooks'
-import './app.css'
+import { useState , useCallback, useEffect,useRef} from 'react'
 
-export function App() {
-  const [lenght, setLenght] = useState(8)
+
+
+function App() {
+  const [length, setLength] = useState(8)
   const [number, setNumber] = useState(false)
-  const [charAllowed, setChar] = useState(false)
-  const [password, setPassword] = useState("")
+  const [character, setChar] = useState(false)
+  const [password , setPassword] = useState("")
 
-  const PasswordGenerator = (useCallback(()=>{
+  //useRef hook
+
+  const passwordRef = useRef(null)
+  const passwordGenerator = useCallback(()=>{
+ 
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    
+
     if(number) str += "0123456789"
-    if(charAllowed) str += "!@#$%^&*~`_-"
-
-    for(let i = 1;i<=lenght;i++){
+    if(character) str += "~`@#$%^&*_-"
+    for(let i =1;i<=length;i++){
       let char = Math.floor(Math.random()*str.length+1)
-      pass += str.charAt(char)
-      console.log(pass);
+       pass += str.charAt(char)
+        
     }
+       setPassword(pass)
     
-    
-
-
-  }, [lenght,number,char,setPassword]))
-
+  },[length,number,character,setPassword])
   
+const copyPassword = useCallback(()=>{
+  passwordRef.current?.select()
+  passwordRef.current?.setSelectionRange(0,length)
+  window.navigator.clipboard.writeText(password)
+},[password])
+
+  useEffect(()=>{
+    passwordGenerator()
+  },[length,number,character,passwordGenerator])
   return (
     <>
-     {/* <h1 className='text-centre text-4xl text-white  py-10'>Password Generator</h1> */}
-     <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700'
-     ><div className='flex shadow rounded-lg overflow-hidden mb-4'>Test</div>
+     <div className='w-full max-w-md mx-auto shadow-md  px-4 py-3 my-8 bg-gray-700  text-orange-500 rounded-lg'>
+      <h1 className=' text-white text-center text-3xl'>Password Generator</h1>
+      <div className= 'flex-shadow overflow-hidden mb-4 flex '>
+        <input type="text"
+        value={password}
+        placeholder='Password'
+        readOnly
+        className='outline-none w-full p-1 px-3  mx-2 '
+        ref={passwordRef}
+         />
+
+        <button className='outline-none text-white bg-blue-600 hover:bg-blue-400 px-3 py-1 font-bold rounded'
+        onClick={copyPassword}>Copy</button>
+      </div>
+      <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center gap-x-1'>
+          <input type="range" min={8} max={20} value={length} className='cursor-pointer ' onChange={(e) => {setLength(e.target.value)} }/>
+          <label>Length:{length}</label>
+        </div>
+        <div className='flex items-center gap-x-1'>
+          <input type="checkbox" defaultChecked = {number} id='numberInput' onChange={()=>{setNumber((prev)=>!prev)}} />
+          <label>Number{number}</label>
+        </div>
+
+        <div className='flex items-center gap-x-1'>
+          <input type="checkbox" defaultChecked = {character} id='numberInput' onChange={()=>{setChar((prev)=>!prev)}} />
+          <label>Character{character}</label>
+        </div>
+      </div>
      </div>
     </>
   )
 }
+
+export default App
